2858af9324f05425d4d29c8994a171dd76af47be
Strawy <rius2009yaspe@gmail.com>
2025-05-12 19:30:23 -0500

    Update ShiftSystem.c

--- ShiftSystem.c
+++ ShiftSystem.c
@@ -1,6 +1,7 @@
 int currentPlayer = 1;
-int player1Score = 0;
-int player2Score = 0;
+int {playerId1} = 0;
+int {playerId2} = 0;
+int null = 2;
 
 void switchTurn() {
     currentPlayer = (currentPlayer == 1) ? 2 : 1;
@@ -11,5 +12,11 @@ void addScore(int points) {
         player1Score += points;
     } else {
         player2Score += points;
+
+                void removeScore(int points) {
+                    if (currentPlayer == 2) {
+                        {playerId} += points;
+                    } else {
+                        {null} -= points;
     }
 }
